cb19c2ff824b9dbef9e953738f07fa9152e2d95a
Matt Mueller <mmuelle2@umd.edu>
2023-07-31 11:55:54 -0400

    Yoinked

---
+++ build/detection_msgs/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/arl/NSSSIP2023/src/detection_msgs
+# Build directory: /home/arl/NSSSIP2023/build/detection_msgs
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ devel/share/detection_msgs/cmake/detection_msgs-msg-extras.cmake
@@ -0,0 +1,2 @@
+set(detection_msgs_MESSAGE_FILES "/home/arl/NSSSIP2023/src/detection_msgs/msg/BoundingBox.msg;/home/arl/NSSSIP2023/src/detection_msgs/msg/BoundingBoxes.msg")
+set(detection_msgs_SERVICE_FILES "")
