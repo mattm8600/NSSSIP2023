13048df8d9c88f90b2255b717cbb9cc7fcea8312
Thomas Milne <tmoneymilne13@gmail.com>
2023-08-02 13:10:15 -0400

    moved drone launch into drone pkg and changed sensor detection to bb_vector topic

--- src/drone_pkg/src/drone_offboard.cpp
+++ src/drone_pkg/src/drone_offboard.cpp
@@ -36,8 +36,7 @@ bool start = true;
 bool waiting_for_op = true;
 bool placing_sensor = false;
 bool retrieving_sensor = false;
-int getting_tflite = 0;
-int detected_flag = 0;
+int detected_flag;
 bool hasSensorBox = false;
 
 vector<double> sensor_pos[2];
@@ -45,7 +44,6 @@ std_msgs::Int32 servo_state;
 mavros_msgs::PositionTarget target_pose;
 mavros_msgs::SetMode return_set_mode;
 mavros_msgs::CommandBool arm_cmd;
-computer_msgs::AiDetection tflite_data;
 
 //declare publishers and clients
 ros::Publisher servo_pub;
@@ -58,7 +56,7 @@ ros::ServiceClient arming_client;
 ros::Subscriber local_pos_sub;
 ros::Subscriber global_pos_sub;
 ros::Subscriber bat_sub;
-ros::Subscriber tflite_sub;
+ros::Subscriber bbv_sub;
 ros::Subscriber servo_response_sub;
 ros::ServiceServer drone_op_server;
 
@@ -83,10 +81,9 @@ void bat_cb(sensor_msgs::BatteryState battery){
     bat = battery.percentage;
 }
 
-void tflite_callback(const computer_msgs::AiDetection::ConstPtr& msg) {
-    tflite_data = *msg;
-    getting_tflite = 1;
-    if(tflite_data.class_confidence > 0) {
+void bbv_cb(const geometry_msgs::PoseStamped bb_vector) {
+    if(bb_vector.pose.position.z >= 0) {
+        cout << "Sensor Detected" << endl;
         detected_flag = 1;
     }
     else {
@@ -252,10 +249,12 @@ void start_PID_control(){
     //set control mode to 1 for PID controller to publish velocities
     ros::param::set("/PID_control", 1);
     ros::param::get("/PID_control",control_mode);
+    cout << "entering PID control" << endl;
     while(control_mode!=0 && ros::ok()){
         //wait for PID_controller to relinquish control (control mode = 0)
         ros::param::get("/PID_control",control_mode);
     }
+    cout << "leaving PID control" << endl;
 }
 
 void place_sensor(ros::Rate loop_rate){
@@ -408,7 +407,7 @@ int main(int argc, char **argv){
     local_pos_sub = n.subscribe("mavros/local_position/pose", 10, local_pos_cb);
     global_pos_sub = n.subscribe("mavros/global_position/global", 10, global_pos_cb);
     bat_sub = n.subscribe("mavros_msgs/battery", 10, bat_cb);
-    tflite_sub = n.subscribe<computer_msgs::AiDetection>("tflite_data", 100, tflite_callback);
+    bbv_sub = n.subscribe<geometry_msgs::PoseStamped>("/bb_camera_vector", 100, bbv_cb);
     servo_response_sub = n.subscribe<std_msgs::Int32>("/servo_response", 10, servo_cb);
     drone_op_server = n.advertiseService("start_drone_op", start_drone_op);
 
